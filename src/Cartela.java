e73798c3a6040f86dea8ec4ce143db4b8974f2f8
armanithiago <armani.thiago@gmail.com>
2013-10-09 22:38:37 -0300

    22:38  9/10/2013

--- src/Cartela.java
+++ src/Cartela.java
@@ -1,11 +1,28 @@
+import java.util.Arrays;
+import java.util.Random;
+
 
 public class Cartela {
-	
+
 	private Cliente cliente;
-	private int[] numeros;
-	
+	private int[] numeros = new int[5];
+
 	public Cartela(Cliente cliente, int[] numeros){
 		this.cliente = cliente;
 		this.numeros = numeros;		
+	}
+
+	public Cartela(Cliente cliente){
+		this.cliente = cliente;
+		gerarNumeros();
+	}
+
+	public void gerarNumeros(){
+		Random r = new Random();
+
+		for(int i = 0; i < numeros.length; i++){
+			numeros[i] = r.nextInt(100);
+		}
+		Arrays.sort(numeros);
 	}
 }
--- src/PlatiniumSorteio.java
+++ src/PlatiniumSorteio.java
@@ -1,4 +1,6 @@
-import java.util.Date;
+import java.util.*;
+
+import javax.activity.InvalidActivityException;
 
 
 public class PlatiniumSorteio {
@@ -9,15 +11,33 @@ public class PlatiniumSorteio {
 	private boolean possuiGanhador = false;
 	private Cliente ganhador = null;
 	private Cartela[] cartelas;
-	
+	private int[] numeroSorteados = new int[5];
+
 	public PlatiniumSorteio(String nomeSorteio, String codigoSorteio){
 		this.nomeSorteio = nomeSorteio;
 		this.codigoSorteio = codigoSorteio;
 	}
-	
+
 	public void encerrarVendas(){ 
 		faseVendas = false;
 	}
-	
-	
+
+	public void sortear() throws Exception{
+		if(faseVendas == true){throw new InvalidActivityException("O sorteio ainda está nas fase de vendas");}
+		// Gera os 5 números aleatórios.
+		Random r = new Random();
+		for(int i = 0; i < numeroSorteados.length; i++){
+			numeroSorteados[i] = r.nextInt(100);
+		}
+		Arrays.sort(numeroSorteados);
+		
+		//TODO Verifica campeões
+		
+	}
+
+
+}
+
+
+
 }
